312ba1532b39b24e95ea4080b3fa15787c11ee2f
Azoad Ahnaf <azoadahnaf@gmail.com>
2018-02-18 19:47:39 +0600

    Bresenham Circle Drawing

---
+++ BresenhamCircle.m
@@ -0,0 +1,58 @@
+fprintf('The value of radius should be 0<r<10\n');
+
+set(gca,'xtick',-10:1:10);
+set(gca,'ytick',-10:1:10);
+line([-10 10],[0 0],'LineWidth',1.5,'Color','black');
+line([0 0],[-10 10],'LineWidth',1.5,'Color','black');
+hold on;
+axis([-10 10 -10 10]);
+title('Give radius in Command Window');
+grid on;
+
+r = input('Enter the radius of the circle: ');
+tic;
+
+if(r<=0||r>10)
+    fprintf('No circle drawing is possible\n');
+    return;
+end
+
+x0 = 0;
+y0 = r;
+do = 3-(2*r);
+xn = x0;
+yn = y0;
+
+while(xn < yn)
+    
+    plot(xn,yn,'b','linewidth',1.5,'marker','X','MarkerEdgeColor','blue');
+    text(xn,yn,['(' num2str(xn) ',' num2str(yn) ')']);
+    hold on;
+    
+    
+    if(do>=0)
+        dn = do+4*(xn-yn)+10;
+        yn = yn-1;
+        
+    else
+        dn = do+4*xn+6;
+    end
+    
+    xn = xn+1;
+    do = dn;
+    
+    if(xn==yn)
+        plot(xn,yn,'b','linewidth',1.5,'marker','X','MarkerEdgeColor','blue');
+        hold on;
+    end
+    
+end
+
+plot(0,0,'marker','O','MarkerEdgeColor','red','linewidth',3),hold on;
+set(gca,'xtick',-10:1:10);
+set(gca,'ytick',-10:1:10);
+axis([-10 10 -10 10]);
+
+title("Bresenham's Circle Drawing Algorithm");
+grid on;
+toc;
---
+++ bresenham_circle.m
@@ -0,0 +1,42 @@
+% AUTHOR    :CHANDAN KUMAR 
+% TITLE     :BRESENHAM CIRCLE ALGORITHM
+% SUBJECT   :COMPUTER GRAPHICS AND SOLID MODELLING
+% DISCLAIMER:This code draws a cirlce using Bresenham circle algorithm.
+
+function bresenham_circle(a,b,r)
+clc, clear all, clf,close
+a = input('Give the centre X coord : ');     % X coord of centre
+b = input('Give the centre Y coord : ');     % Y coord of centre
+r = input('Give the radius of circle : ');   % Radius of the circle to be drawn
+inc = input('Give the increment step for coordinate[Default:1]: '); % Use the value 
+if isempty(inc)                              % of inc greater than 1
+    inc =1;                                  % usually less than radius
+end
+
+x = 0;
+y = r;
+d= 3-2*r;
+
+while(x < y)
+	funplot(a,b,x,y,r);
+	if d>=0
+		d = d +4*(x-y)+10;
+		x = x + 1/inc;
+		y = y - 1/inc;
+	else
+		d = d + 4*x +6;
+		x = x+1/inc;
+	end 
+end
+function funplot(a,b,p,q,r)
+for i= -1:2:1
+	for j= -1:2:1
+		plot(a+i*p,b+j*q,'r*');
+        plot(a+i*q,b+j*p,'r*');
+		grid on
+		hold on
+	end
+end
+title('Bresenham circle')
+axis([a-r-1 a+r+1 b-r-1 b+r+1])
+axis equal;
